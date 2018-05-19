4936430b61de961ec1ad9a5a85f867aad7a4fd30
Yxz-xidian <hlgyxz@sina.com>
2018-05-19 14:52:42 +0800

    Update IdiomChain.js

--- contract/IdiomChain.js
+++ contract/IdiomChain.js
@@ -46,6 +46,9 @@ IdiomChainContract.prototype = {
 
     //创建一条成语链，判断成语是否存在，存在则不能创建，不存在则创建成语链，并显示该链
     createIdiomChain: function(_idiom, _address, _storageTime) {
+
+        //检查地址是否符合规范
+
         var idiomExist = this.isIdiomExists(_idiom);
         //成语不存在，可以创建
         
@@ -57,7 +60,7 @@ IdiomChainContract.prototype = {
             //添加成语到成语池里
             this.idiomPool.put(this.idiomNumber, player);
             //创建一条成语链，把成语id加进去
-            this.idiomChains.put(this.chainNumber, ""+ this.idiomNumber);
+            this.idiomChains.put(this.chainNumber, "," + this.idiomNumber);
             //成语链总数加1
             this.chainNumber++;
             //成语池总数量加1
@@ -92,6 +95,9 @@ IdiomChainContract.prototype = {
     //查询成语是否存在，存在显示它在的所有链，不存在显示不存在
     checkIdiomInChains: function(_idiom) {
         var idiomExist = this.isIdiomExists(_idiom);
+        if (!idiomExist) {
+            return null;
+        }
         var id;
         for (var i = 0; i < this.idiomNumber; i++) {
             if(_idiom == this.idiomPool.get(i).idiom) {
@@ -101,9 +107,6 @@ IdiomChainContract.prototype = {
         }
         var array = [];//拥有成语的链编号
 
-        if (!idiomExist) {
-            return null;
-        }
         var idStr = "" + id;
         var chainStr;
         for (var i = 0; i < this.chainNumber; i++) {
@@ -126,16 +129,19 @@ IdiomChainContract.prototype = {
 
     //添加成语到某条链，判断成语和该链上的最后一个成语是否符合规则，符合就添加，不符合就返回信息提示
     addIdiomToChain: function(_idiom, _address, _storageTime, _chainId) {
+
+        //检查地址是否符合规范
+
         //判断成语是否符合成语接龙规则,成语不能与成语池里的成语重复
         var str = this.idiomChains.get(_chainId);
         var index = str.lastIndexOf(",");
-        var id = str.substring(index + 1, str.length);
+        var id = str.substring(index + 1);
         
-        if (_idiom.substring(3) != this.idiomPool.get(parseInt(id)).idiom) {
+        if (_idiom.substring(0,1) != this.idiomPool.get(parseInt(id)).idiom.substring(3)) {
             return false;
         } else {
             //成语不能与成语池里的成语重复
-            if (!this.isIdiomExists(_idiom)) {//池中存在
+            if (this.isIdiomExists(_idiom)) {//池中存在
                 return false;
             }
             //成语池中不存在该成语，并且符合该成语链 接龙规则
@@ -173,8 +179,7 @@ IdiomChainContract.prototype = {
     },
     //查询成语是否在某条成语链上，用编号(辅助函数，查询一个id，是否在字符串中)
     isIdiomInChain: function(_id, _idiomChain) {
-        var strs = new Array();
-        strs = _idiomChain.split(",");
+        var strs = _idiomChain.split(",");
         for (var i = 0; i < strs.length; i++) {
             if (_id == strs[i]) {
                 return true;
@@ -185,13 +190,14 @@ IdiomChainContract.prototype = {
 
     //返回一条成语链上的所有成语信息
     getInfoByChain: function(_chainId) {
+
         var result = "[{";
         var chainStr = this.idiomChains.get(_chainId);
         var strs = chainStr.split(",");
 
         var player;
-        for (var i = 0; i < strs.length; i++) {
-            player = this.idiomPool.get(parseInt(str[i]));
+        for (var i = 1; i < strs.length; i++) {
+            player = this.idiomPool.get(parseInt(strs[i]));
             result += JSON.stringify(player) + ",";
         }
         result = result.substring(0, result.length-1);
