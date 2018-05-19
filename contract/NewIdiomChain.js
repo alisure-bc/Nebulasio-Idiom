09e2b8801f7e989a601046b82085f5016ee5a142
Yxz-xidian <hlgyxz@sina.com>
2018-05-19 16:08:25 +0800

    Update NewIdiomChain.js
    比上一个版本添加了一个获取系统时间的时间函数getNowFormateDate，返回时间格式为yyyy-MM-dd HH:MM:SS

--- contract/NewIdiomChain.js
+++ contract/NewIdiomChain.js
@@ -217,6 +217,26 @@ IdiomChainContract.prototype = {
             result = result.substring(0, result.length-1);
             return result;
         }
+    },
+
+    //获取当前系统的日期时间，格式为"yyyy-MM-dd HH:MM:SS"
+    getNowFormateData: function() {
+        var date = new Date();
+        var seperator1 = "-";
+        var seperator2 = ":";
+        var month = date.getMonth() + 1;
+        var strDate = date.getDate();
+        if (month >=1 && month <= 9) {
+            month = "0" + month;
+        }
+        if(strDate >=0 && strDate <=9) {
+            strDate = "0" + strDate;
+        }
+        var currentDate = date.getFullYear() + seperator1 + month 
+                        + seperator1 + strDate + " " + date.getHours() 
+                        + seperator2 + date.getMinutes() + seperator2 
+                        + date.getSeconds();
+        return currentDate;
     }
 };
 
